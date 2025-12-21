b85ca56f0b4fdf4eff58e11bf2e3be4e4dc16f39
Netix Solutions <email@netixsolutions.com>
2025-12-21 18:39:57 +0000

    make sure descriptions all take up an equal amount of vertical space wit

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -113,7 +113,7 @@ function PricingCard({ product, onPurchase, isPurchasing, isFeatured }: { produc
             )}
             <CardHeader className="text-center">
                 <CardTitle className="font-headline text-2xl">{product.name}</CardTitle>
-                {product.description && <CardDescription className="pt-2">{product.description}</CardDescription>}
+                {product.description && <CardDescription className="pt-2 min-h-[4rem]">{product.description}</CardDescription>}
             </CardHeader>
             <CardContent className="flex-grow flex flex-col">
                  <div className="flex justify-center my-6">
@@ -265,3 +265,5 @@ export default function PricingPage() {
     </div>
   );
 }
+
+    
